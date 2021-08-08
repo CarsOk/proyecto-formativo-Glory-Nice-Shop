02c07804456e82c9a092dc1d1fe34f14ac115836
Miguel Jimenez <migueljiarteta@gmail.com>
2021-08-08 17:04:51 -0500

    se creo estrucutura para que el usuario se registre e inicie sesion en el aplicativo

--- rails/db/schema.rb
+++ rails/db/schema.rb
@@ -10,17 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_29_021746) do
+ActiveRecord::Schema.define(version: 2021_08_06_185225) do
 
-  create_table "users", force: :cascade do |t|
-    t.string "nombre1"
-    t.string "nombre2"
-    t.string "apellido1"
-    t.string "apellido2"
-    t.integer "celular"
-    t.string "email"
+  create_table "usuarios", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "nombres"
+    t.string "apellidos"
+    t.integer "celular"
+    t.index ["email"], name: "index_usuarios_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_usuarios_on_reset_password_token", unique: true
   end
 
 end
